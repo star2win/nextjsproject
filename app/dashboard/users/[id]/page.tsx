1d3907e7dd4e75fdd9000769a7778a21a3e645c7
Filip Szymanski <fszymanski@gmail.com>
2025-04-28 11:19:38 -0700

    Updated examples

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>USER DETAILS PAGE</div>
+  )
+}
+
+export default User
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  // Configuration for development server
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
 };
 
 export default nextConfig;
